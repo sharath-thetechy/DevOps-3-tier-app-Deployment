1e6b442e91db2fc9958ab203f2cc64d5e1369b9b
sharath-thetechy <sharathchandragupta193@gmail.com>
2025-07-16 20:11:15 +0530

    Update provider.tf

--- terra-config/provider.tf
+++ terra-config/provider.tf
@@ -11,3 +11,11 @@ terraform {
 provider "aws" {
   region = "ap-south-1"
 }
+
+resource "aws_ecr_repository" "frontend" {
+  name = "frontend"
+}
+
+resource "aws_ecr_repository" "backend" {
+  name = "backend"
+}
