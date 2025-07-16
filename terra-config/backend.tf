17c9a3985d15af0048b15a44183d2e3a44d3fc7c
sharath-thetechy <sharathchandragupta193@gmail.com>
2025-07-16 17:16:49 +0530

    Update backend.tf

--- terra-config/backend.tf
+++ terra-config/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "sharuss3"  # Change if the name already exists. 
-    key            = "eks/terraform.tfstate"       
+    key            = "prod/terraform.tfstate"       
     region         = "ap-south-1"                   
     encrypt        = true
   }
