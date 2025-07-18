d2bc60902ffd5e01e60280548c7e9df55d55be47
sharath-thetechy <sharathchandragupta193@gmail.com>
2025-07-18 14:48:38 +0530

    Update eks.tf

--- terra-config/eks.tf
+++ terra-config/eks.tf
@@ -6,7 +6,9 @@ resource "aws_eks_cluster" "eks_cluster" {
   role_arn = aws_iam_role.cluster_role.arn
 
   vpc_config {
-    subnet_ids = data.aws_subnets.public.ids
+    subnet_ids = [
+      "subnet-097fbc464371145d4",
+      "subnet-0d05f656e4bcfb0c2"
   }
   depends_on = [
     aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
